107101a110bb060151ffe819ff3d60706f0a25e3
Conrad Dean <conrad.p.dean@gmail.com>
2019-07-21 10:09:52 -0400

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-pub fn fetch_one_row<F, T, E>(mut m: rusqlite::MappedRows<F>) -> Result<T, E>
+use rusqlite::Result;
+
+pub fn fetch_one_row<F, T>(mut m: rusqlite::MappedRows<F>) -> Result<T>
 where
-    F: FnMut(&rusqlite::Row<'_>) -> Result<T, E>,
+    F: FnMut(&rusqlite::Row<'_>) -> Result<T>,
 {
     m.next().unwrap()
 }
